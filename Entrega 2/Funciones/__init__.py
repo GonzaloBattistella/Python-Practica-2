7d60f9f0ac40728bd01f1afa4d0e53c1a0f51709
Gonzalo Battistella <gonzalobatti01@gmail.com>
2024-04-08 21:04:35 -0300

    Se modificaron los archivos de la entrega 2

--- Entrega 2/Funciones/__init__.py
+++ Entrega 2/Funciones/__init__.py
@@ -1,15 +1,3 @@
-jugadores = {
-    "Names" : ["Agustin"," Yanina", "Andrés", "Ariadna", "Bautista", "CAROLINA",
-"CESAR", "David", "Diego", "Dolores", "DYLAN", "ELIANA", "Emanuel", "Fabián", "Noelia",
-"Francisca", "FEDERICO", "Fernanda", "GONZALO", "Nancy"],
-    "Goals" : [0, 10, 4, 0, 5, 14, 0, 0, 7, 2, 1, 1, 1, 5, 6, 1, 1, 2, 0,
-11],
-    "Goals_avoided" : [0, 2, 0, 0, 5, 2, 0, 0, 1, 2, 0, 5, 5, 0, 1, 0, 2,
-3, 0, 0],
-    "Assists" : [0, 5, 1, 0, 5, 2, 0, 0, 1, 2, 1, 5, 5, 0, 1, 0, 2, 3, 1,
-0]
-}
-
 
 #Funcion para determinar el goleador/a. Inciso 2
 def info_goleador(info_jugadores):
